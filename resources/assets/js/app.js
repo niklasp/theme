3fd5152ca30686fa6a0b4267ce46facf8e58b6a9
Niklas Jurij Plessing <contact@niklasplessing.net>
2021-03-18 21:23:09 +0100

    remove unneded jquery

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,2 @@
-// Theme by default loads a jQuery as dependency of the main script.
-// Let's include it using ES6 modules import.
-import $ from 'jquery'
+// Let's include anything you need using ES6 modules import. E.g.
+// import { pick } from 'lodash';
